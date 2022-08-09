d977cdcfd4f5648eb2c59c1c7980ed0e46e403f9
fishbone222 <github@fischercortizo.com>
2022-08-09 10:27:45 +0200

    Update pre-install.sh

--- centos/pre-install.sh
+++ centos/pre-install.sh
@@ -5,7 +5,7 @@ dnf -y install git
 dnf config-manager --set-enabled powertools -y
 
 #get the install script
-cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
+cd /usr/src && git clone https://github.com/fishbone222/fusionpbx-install.sh.git
 
 #change the working directory
 cd /usr/src/fusionpbx-install.sh/centos
